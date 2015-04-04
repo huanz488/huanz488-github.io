2f8e7ac6b6b01cb4452fdc12c2a517597cd0b8db
huanz488 <huanz488@newschool.edu>
2015-04-04 15:57:05 +0000

    Adding index file

---
+++ loop/loop.js
@@ -0,0 +1,77 @@
+var crab = [
+	" ◺ ◿             ◺ ◿", 
+	"  /  ◉         ◉ /", 
+	"╍ ═ ◿ ▩ ▩ ▩ ▩ ▩ ◺ ═ ╍", 
+	"╍ ═ ▩ ▩ ▩ ▩ ▩ ▩ ▩ ═ ╍", 
+	" ╍ ═◹ ▩ ▩ ▩ ▩ ▩ ◸═ ╍ ", 
+	"          ◍  ",
+	"            ◍  ",
+	"              ◍  ",
+	"            ◍  ",
+	"          ◍  ",
+	"         ◺ ◿         ◺ ◿", 
+	"          /  ◉      ◉ /", 
+	"      ╍ ═ ◿ ▩ ▩ ▩ ▩ ▩ ◺ ═ ╍", 
+	"      ╍ ═ ▩ ▩ ▩ ▩ ▩ ▩ ▩ ═ ╍", 
+	"       ╍ ═◹ ▩ ▩ ▩ ▩ ▩ ◸═ ╍", 
+	"                ◍  ",
+	"                  ◍  ",
+	"                   ◍  ",
+	"                  ◍  ",
+	"                ◍  ",
+	"             ◺ ◿           ◺ ◿", 
+	"             /  ◉         ◉ /", 
+	"           ╍ ═ ◿ ▩ ▩ ▩ ▩ ▩ ◺ ═ ╍", 
+	"           ╍ ═ ▩ ▩ ▩ ▩ ▩ ▩ ▩ ═ ╍", 
+	"            ╍ ═◹ ▩ ▩ ▩ ▩ ▩ ◸═ ╍ ", 
+	"                     ◍  ", 
+	"                       ◍  ", 
+	"                        ◍  ", 
+	"                       ◍  ", 
+	"                     ◍  ", ]
+
+for (var index=0; index <=crab.length; index++){
+	console.log (crab [index]);
+}
+
+for (var index =crab.length; index >=0; index--){
+	console.log (crab [index]);
+}
+
+for (var index=0; index <=crab.length; index++){
+	console.log (crab [index]);
+}
+
+for (var index =crab.length; index >=0; index--){
+	console.log (crab [index]);
+}for (var index=0; index <=crab.length; index++){
+	console.log (crab [index]);
+}
+
+for (var index =crab.length; index >=0; index--){
+	console.log (crab [index]);
+}
+
+for (var index=0; index <=crab.length; index++){
+	console.log (crab [index]);
+}
+
+for (var index =crab.length; index >=0; index--){
+	console.log (crab [index]);
+}
+
+for (var index=0; index <=crab.length; index++){
+	console.log (crab [index]);
+}
+
+for (var index =crab.length; index >=0; index--){
+	console.log (crab [index]);
+}
+
+for (var index=0; index <=crab.length; index++){
+	console.log (crab [index]);
+}
+
+for (var index =crab.length; index >=0; index--){
+	console.log (crab [index]);
+}
