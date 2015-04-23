ec2f7889cf064baf5ce09b6340e3ce8893090522
huanz488 <huanz488@newschool.edu>
2015-04-23 07:49:02 +0000

    Adding index file

--- image generator/application.js
+++ image generator/application.js
@@ -8,10 +8,30 @@ var kumamouseimgs = [
                  
                 ];
 
+var quotes =
+["Hi.",
+"I am Rilakkuma.",
+"I am hungry.",
+"I want something to eat.",
+
+];
 
 
 
+var selectedquote = false;
 
+$(".kumamouse").click(function() {
+  
+  var nextquote = quotes[Math.floor(Math.random()*quotes.length)];
+  
+  while (selectedquote == nextquote) {
+    nextquote = quotes[Math.floor(Math.random()*quotes.length)];
+  }
+  
+  selectedquote = nextquote;
+  
+  $(".quote-holder").html(nextquote);
+  
 $('.kumamouse').click(function() {
     var random = Math.ceil(Math.random() * kumamouseimgs.length);
     $(".backgroundtest").css({ 'background-image': 'url("images/' + kumamouseimgs[random] + '")' });
@@ -24,4 +44,5 @@ document.getElementById("kumamouse").play();
 
 $('.kumamouse').mouseleave(function() {
 document.getElementById("kumamouse").pause(); 
+});
 });
